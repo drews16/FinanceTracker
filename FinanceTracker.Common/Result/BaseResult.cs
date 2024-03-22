442ca91a8591063b750d9c0812ee4f421fbc29d3
drews16 <109108583+drews16@users.noreply.github.com>
2024-03-22 21:50:54 +0400

    Add Common project

---
+++ FinanceTracker.Common/Result/BaseResult.cs
@@ -0,0 +1,14 @@
+﻿namespace FinanceTracker.Common.Result
+{
+    public class BaseResult
+    {
+        public virtual bool IsSuccess => !ErrorMessages.Any();
+        public IEnumerable<string> ErrorMessages { get; init; } = Enumerable.Empty<string>();
+    }
+
+    public sealed class BaseResult<T> : BaseResult
+    {
+        public override bool IsSuccess => base.IsSuccess;
+        public T? Result { get; init; }
+    }
+}

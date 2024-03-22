d6df3522258767979655eaf408f0d990af205833
drews16 <109108583+drews16@users.noreply.github.com>
2024-03-22 22:04:42 +0400

    Add infrastructure project

---
+++ FinanceTracker.DAL/Database/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using FinanceTracker.Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace FinanceTracker.DAL.Database
+{
+    public sealed class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Account> Accounts { get; }
+        public DbSet<Transaction> Transactions { get; }
+        public DbSet<User> Users { get; }
+        public DbSet<Category> Categories { get; }
+    }
+}
